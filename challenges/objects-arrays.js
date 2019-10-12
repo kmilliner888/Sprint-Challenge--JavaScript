948260235edc912cd040c3ef67421aa0bfb147a5
Kaylen Milliner <kaylen.milliner@gmail.com>
2019-10-11 23:52:28 -0400

    working through arrays

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -12,23 +12,39 @@
 
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
 
+const tyrannosaurus = {diet: "carnivorous", weight: "7000kg", length: "12m", period: "Late Cretaceous"};
+
+const stegosaurus = {diet: "herbivorous", weight: "2000kg", length: "9m", period: "Late Jurassic"};
+
+const velociraptor = {diet: "carnivorous", weight: "15kg", length: "1.8m", period: "Late Cretaceous"};
+
+
+
+
+
+
+
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(tyrannosaurus.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(velociraptor.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(stegosaurus.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(tyrannosaurus.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+
+tyrannosaurus.roar = function() {
+  return `RAWERSRARARWERSARARARRRR!`
+}
+console.log(tyrannosaurus.roar());
 
 
 // ==== Arrays ====
@@ -51,21 +67,40 @@ const graduates = [
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array. This will be an array of strings.
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
-const universities = [];
-console.log(universities);
+const universities = graduates.map((schoolName) => {
+  return `${schoolName.university}`;
+});
+console.log(universities.sort());
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
+
 
 The resulting contact information strings should have a space between the first name and the email, like this: 
 "Josh josh@example.com"
 
 Log the result of your new array. */
-const contactInfo = [];
+
+const contactInfo = graduates.map(contact => {
+  return `${contact.first_name} ${contact.email}`
+});
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
-const unisWithUni = [];
-console.log(unisWithUni);
+// const unisWithUni = function uniName(" ",  cb) {
+//   console.log(unisWithUni);
+//   if(graduates.includes("Uni")) {
+//     return cb(true);
+//   }
+//   else {
+//     return cb(false);
+//   }
+// };
+
+const unisWithUni = graduates[graduates.university];
+
+
+// console.log(unisWithUni.includes("Uni"));
+// console.log(unisWithUni());
 
 
 // ==== ADVANCED Array Methods ====
@@ -87,10 +122,20 @@ const zooAnimals = [
 
 /* Request 1: .forEach()
 
-The zoos want to display both the scientific name and the animal name in front of the habitats. Populate the displayNames array with only the animal_name and scientific_name of each animal. displayNames will be an array of strings, and each string should follow this pattern: "Name: Jackal, asiatic, Scientific: Canis aureus."
+The zoos want to display both the scientific name and the animal name in front of the habitats. 
+// Populate the displayNames array with only the animal_name and scientific_name of each animal. 
+// displayNames will be an array of strings, and each string should follow this pattern: 
+//"Name: Jackal, asiatic, Scientific: Canis aureus."
 
 */
-const displayNames = [];
+const displayNames = zooAnimals.forEach((name) => {
+  console.log(name);
+  return `Name: ${name.animal_name}, Scientific: ${name.scientific_name}`
+
+}
+
+);
+
 console.log(displayNames);
 
 /* Request 2: .map()
